bbf0acc62f3caa3dfc0b7781fe8cf9648916a0e5
c0untfl0yd <c0untfl0yd@mail.bg>
2019-11-25 21:23:23 -0800

    removing trash folder debug complete for now

---
+++ README.md
@@ -0,0 +1,2 @@
+# nes_test
+js_nes_test (roms in unit8array)
--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <?php 
-
-	echo "echoes of tortured souls";
-
+	echo "test";
 ?>
-
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="description" content="">
+	<script type="text/javascript" src="js/lib/jquery-1.8.0.js"></script>
+	<script type="text/javascript" src="build/cycloa.js"></script>
+<style>
+canvas {	
+	position: absolute;
+	top:0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+  	
+	margin: auto;
+}
+</style>
+</head>
+<body>
 <script>
-alert(1);
+
 </script>
+
+	
+		<select id="rom_sel" style="display:none">
+		</select>
+		
+		<div class="nes_screen_wrapper" id="nes_screen_wrapper">
+			<canvas id="nes_screen" width="256" height="240"></canvas>
+		</div>
+		
+<script>
+
+</script>
+</body>
+</html>
