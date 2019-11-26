9a8ab06f4d84b75651853b19ffbb01bb81eefeee
c0untfl0yd <c0untfl0yd@mail.bg>
2019-11-25 20:08:11 -0800

    added all to trash folder to debug

--- index.php
+++ index.php
@@ -1,39 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title></title>
-	<meta name="description" content="">
-	<script type="text/javascript" src="js/lib/jquery-1.8.0.js"></script>
-	<script type="text/javascript" src="build/cycloa.js"></script>
-<style>
-canvas {	
-	position: absolute;
-	top:0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-  	
-	margin: auto;
-}
-</style>
-</head>
-<body>
-<?php echo "test";?>
-<script>
+<?php 
 
-</script>
+	echo "echoes of tortured souls";
 
-	
-		<select id="rom_sel" style="display:none">
-		</select>
-		
-		<div class="nes_screen_wrapper" id="nes_screen_wrapper">
-			<canvas id="nes_screen" width="256" height="240"></canvas>
-		</div>
-		
-<script>
-
-</script>
-</body>
-</html>
+?>
